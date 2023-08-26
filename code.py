164156027687a3eac66cff1ae9a33c70f73bd2b0
Vp127 <119748571+Vp127@users.noreply.github.com>
2023-08-26 18:04:57 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from lyrics_extractor import SongLyrics
  
-# user defined function
+
 def get_lyrics():
    
     extract_lyrics = SongLyrics(
@@ -12,16 +12,15 @@ def get_lyrics():
     result.set(res)
  
  
-# object of tkinter
+
 master = Tk()
 master.title("Lyrics Extractor")
 master.configure(bg='light grey')
  
-# Variable Classes in tkinter
+
 result = StringVar()
  
-# Creating label for each information
-# name using widget Label
+
 Label(master, text="Enter Song name : ",
       bg="light grey").grid(row=0, sticky=W)
  
@@ -29,15 +28,14 @@ Label(master, text="Result :",
       bg="light grey").grid(row=3, sticky=W)
  
  
-# Creating label for class variable
-# name using widget Entry
+
 Label(master, text="", textvariable=result,
       bg="light grey").grid(row=3, column=1, sticky=W)
  
 e = Entry(master, width=50)
 e.grid(row=0, column=1)
  
-# creating a button using the widget
+
 b = Button(master, text="Show",
            command=get_lyrics, bg="Purple")
  
